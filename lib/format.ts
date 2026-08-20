2be6d9d4b0b9450dfabd2506565b04d95cc4a211
eroluis <eroluis@gmail.com>
2026-08-19 21:12:20 -0400

    Delete lib directory

--- lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function dinero(n: number): string {
-  return `$${Math.round(n).toLocaleString("es-CL")}`
-}
-
-export function numero(n: number): string {
-  return Math.round(n).toLocaleString("es-CL")
-}
